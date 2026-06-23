91cbd9f7add1439a7cb8b3c6a8eb0fa811fd2659
airibarren <anadelfinairibarren@gmail.com>
2026-06-22 21:18:29 -0300

    fix gallery images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,48 +1,32 @@
 import { useEffect, useState } from "react"
 
-import foto1 from "../assets/foto1.jpg"
-import foto2 from "../assets/foto2.jpg"
-import foto3 from "../assets/foto3.jpg"
-import foto4 from "../assets/foto4.jpg"
-import foto5 from "../assets/foto5.jpg"
-
 function Gallery() {
-
   const photos = [
-    foto1,
-    foto2,
-    foto3,
-    foto4,
-    foto5
+    "/images/foto1.jpg",
+    "/images/foto2.jpg",
+    "/images/foto3.jpg",
+    "/images/foto4.jpg",
+    "/images/foto5.jpg",
   ]
 
   const [current, setCurrent] = useState(0)
 
   // AUTO SLIDE
-
   useEffect(() => {
-
     const interval = setInterval(() => {
-
       setCurrent((prev) => (prev + 1) % photos.length)
-
     }, 1500)
 
     return () => clearInterval(interval)
-
-  }, [photos.length])
+  }, [])
 
   return (
-
     <section
       id="galeria"
       className="bg-[#F6F1E8]"
     >
-
       {/* FOTO */}
-
       <div className="w-full overflow-hidden">
-
         <img
           src={photos[current]}
           alt={`Foto del book ${current + 1}`}
@@ -54,13 +38,9 @@ function Gallery() {
             duration-2000
           "
         />
-
       </div>
-
     </section>
-
   )
-
 }
 
 export default Gallery
